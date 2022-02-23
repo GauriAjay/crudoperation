a2fe15eb43efde98f2d9b9a004859766e4aa4c5a
gauri <gauriajay18@gmail.com>
2022-02-23 23:19:23 +0530

    update changes

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
  include 'connect.php';
-if(isset($_GET['updateid']))
+if(isset($_POST['updateid']))
 {
-$id=$_GET['updateid'];
+$id=$_POST['updateid'];
 
  $sql="select * from `crud` where id=$id";
  $result=mysqli_query($con,$sql);
@@ -46,19 +46,19 @@ if($result){
     <form method="post"> 
 <div class="form-group">
     <label>Name</label>
-    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off" value=<?php echo $name;?>>
+    <input type="text" class="form-control" placeholder="Enter your name" name="name" autocomplete="off" value="<?php echo $name;?>">
 </div>
 <div class="form-group">
     <label>Email</label>
-    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off" value=<?php echo $email;?>>
+    <input type="email" class="form-control" placeholder="Enter your email" name="email" autocomplete="off" value="<?php echo $email;?>">
 </div>
 <div class="form-group">
     <label>Mobile</label>
-    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off" value=<?php echo $mobile;?>>
+    <input type="text" class="form-control" placeholder="Enter your mobile number" name="mobile" autocomplete="off" value="<?php echo $mobile;?>">
 </div>
 <div class="form-group">
     <label>Password</label>
-    <input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off" value=<?php echo $password;?>>
+    <input type="text" class="form-control" placeholder="Enter your password" name="password" autocomplete="off" value="<?php echo $password;?>">
 </div>
     
   <button type="submit" class="btn btn-primary" name="submit">Update</button>
